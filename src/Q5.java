cb6f6ed07ee88259e9309c68d842fb30280cb92c
chopragauri <chopra.gauri06@gmail.com>
2025-10-24 18:58:06 +0530

    q5

--- src/Q5.java
+++ src/Q5.java
@@ -1,2 +1,21 @@
+import java.io.*;
+
 public class Q5 {
+    public static void main(String[] args) {
+        File file = new File("src/productNames.txt");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            System.out.println("Product Names in Uppercase:");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line.toUpperCase());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error in reading file: " + e.getMessage());
+        }
+    }
 }
